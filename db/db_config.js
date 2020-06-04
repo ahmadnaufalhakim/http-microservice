c3d2a715c6eb702315ffb267322a830ab0879f8a
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2020-06-04 12:20:40 +0700

    add docker container env variables

--- db/db_config.js
+++ db/db_config.js
@@ -1,6 +1,11 @@
+const isDocker = require('is-docker');
+require('dotenv').config('./../');
+
 const config = {
 	// Match with the db container configuration
-	host: 'mariadb',
+	host: isDocker()
+		? process.env.DOCKER_DATABASE_HOST
+		: 'localhost',
 	port: 3306,
 	user: 'root',
 	password: 'root',
--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 const indexRouter = require('./routes/index');
 const studentRouter = require('./routes/students/students');
-
-// pool.getConnection()
-//   .then((conn) => {
-//     console.log(conn);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// console.log(pool);
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
